550c813b07512ad6d832a0a7f4e2759d408f0e05
Grisha <parfen20@rambler.ru>
2022-10-26 20:53:21 +0200

    add transactiont tests

--- mywallets/walletsapi/tests/test_wallet.py
+++ mywallets/walletsapi/tests/test_wallet.py
@@ -1,8 +1,7 @@
-import decimal
-
 from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APITestCase
+
 
 class WalletApiTestCase(APITestCase):
 
